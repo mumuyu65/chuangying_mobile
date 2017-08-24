a3ba7ef371e6e4241b8807c5398945f3674f3e42
chenlong <chenlongwill@qq.com>
2017-08-24 17:39:29 +0800

    解决微信中打开的文本框遮盖问题

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -99,6 +99,15 @@ export default {
     mounted (){
         this.initChat();   //判断是否登录
         $("#chat_content").css("bottom",'0rem');
+
+        //微信内置浏览器浏览H5页面弹出的键盘遮盖文本框的解决办法
+        window.addEventListener("resize", function () {
+            if (document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA") {
+            window.setTimeout(function () {
+                document.activeElement.scrollIntoViewIfNeeded();
+                }, 0);
+            }
+        })
     },
     methods:{
         //聊天图标
